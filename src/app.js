ab85d187dec3b6457d2737efa6023fe96cdbfbef
revatagaon <prvn.gr11@gmail.com>
2025-07-04 12:31:43 +0530

    Modified App

--- src/app.js
+++ src/app.js
@@ -2,32 +2,20 @@ const express = require("express")
 
 const app = express();
 
-// In Express 4.x.x version if you use "app.use" will not work as expected but in the Express version 5.1.0 you need use app.get method
-
-// app.use("/",(req, res)=>{
-//   res.send("Hello World from Server")
-// })
-
-// app.use("/hello", (req, res) => {
-//   res.send("Hello Hello Hello Hello");
-// });
-
-// app.use("/get",(req, res)=>{
-//   res.send("Hello from GET request, its not working");
-// })
-
-app.get("/",(req, res)=>{
-  res.send("Hello World from Server")
-})
-
-app.get("/hello", (req, res) => {
+app.use("/hello", (req, res) => {
   res.send("Hello Hello Hello Hello");
 });
 
-app.get("/get",(req, res)=>{
+app.use("/get",(req, res)=>{
   res.send("Hello from GET request, its not working");
+})
+
+app.use("/",(req, res)=>{
+  res.send("Hello World from Server")
 })
 
 app.listen(7777, ()=>{
   console.log("Server is successfully running on port 3000...")
 })
+
+// The order of the app.use matters, and it executes the app.use in a synchronous way
