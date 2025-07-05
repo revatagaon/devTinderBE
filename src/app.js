ee433649fac33dc4fab6ac49b48971fd86b64bee
revatagaon <prvn.gr11@gmail.com>
2025-07-05 15:41:56 +0530

    Implemented the middleware and did error handling

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-const express = require("express")
+const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
@@ -36,43 +37,79 @@ const app = express();
 // app.use("route", rH1, rH2, rH3, rH4, [rH5, rH6], rH7)
 // app.use("route", rH1, rH2, [rH3, rH4, rH5], rH6, rH7)
 // app.use("route", [rH1, rH2, rH3, rH4], rH5, rH6, rH7)
-app.use("/user", [
-  (req, res, next) => {
-    console.log("Reuote Handler 1")
-    // res.send("1st Response")
-    next();
-  },
-  (req, res, next) => {
-    console.log("Reuote Handler 2")
-    // res.send("2nd Response")
-    next();
-  },
-  (req, res, next) => {
-    console.log("Reuote Handler 3")
-    // res.send("3rd Response")
-    next();
-  },
-  (req, res, next) => {
-    console.log("Reuote Handler 4")
-    // res.send("4th Response")
-    next();
-  },
-  (req, res, next) => {
-    console.log("Reuote Handler 5")
-    res.send("5th Response")
-    next();
-  },
-]
-)
+
+// app.use("/user", [
+//   (req, res, next) => {
+//     console.log("Reuote Handler 1")
+//     // res.send("1st Response")
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("Reuote Handler 2")
+//     // res.send("2nd Response")
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("Reuote Handler 3")
+//     // res.send("3rd Response")
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("Reuote Handler 4")
+//     // res.send("4th Response")
+//     next();
+//   },
+//   (req, res, next) => {
+//     console.log("Reuote Handler 5")
+//     res.send("5th Response")
+//     next();
+//   },
+// ]
+// )
 
 // This route will match all the HTTP API request that starts with /test
 // The following route will match all the HTTP methods like GET, POST, PUT, DELETE etc
-app.use("/test", (req, res) => {
-  res.send("Hello from test method");
-});
+// app.use("/test", (req, res) => {
+//   res.send("Hello from test method");
+// });
+
+// For Admin checking
+
+app.use("/admin", adminAuth)
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("Got all the User Data")
+})
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted a User")
+})
+
+app.use("/user", userAuth, (req, res) => {
+  res.send("User data sent")
+})
+
+// Error Handling
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong")
+  }
+})
+
+app.use("/getUserData", (req, res) => {
+  try {
+    throw new Error("This is a test error")
+    res.send("User data sent")
+  } catch (err) {
+    res.status(500).send("Some error contact support team")
+  }
+})
 
 app.listen(7777, () => {
   console.log("Server is successfully running on port 3000...")
 })
 
 // The order of the app.use matters, and it executes the app.use in a synchronous way
+
+// The app.use is used to handle the middleware, and it can be used to handle the request and response in a synchronous way
+// The function which sends the response is known as Route Handler and rest of the functions are known as Middleware
